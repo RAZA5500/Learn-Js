d60966132095e950d5d817221b4ed0d07b2e42d3
RAZA5500 <ytakraza@gmail.com>
2025-12-15 18:38:35 +0500

    New Practice

---
+++ Js-Book-Practice/bookch2.js
@@ -0,0 +1,27 @@
+// var userName = "Raza"
+
+// var myName = "RazaHasan"
+
+// var message = "Hello World!"
+// alert(message)
+
+// var studentName = "Raza"
+// var studentAge = "17 Year OLd"
+// var studentCertified = "Certified Mobile Application Development"
+// alert(studentName)
+// alert(studentAge)
+// alert(studentCertified)
+
+// var message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(message)
+
+// var message = "My email addres is "
+// var email = "Raza@example.com"
+// var final = message + email
+// alert(final) 
+
+// var book = " a smarter way to learn javascript"
+// alert("I am trying to learn from the book" + book)
+
+var write = "Yah ! I can write HTML content through JavaScript"
+document.writeln(write)
